050f2f1b1df396ce4b573ff065503d1467de7e8b
mdoury <maxime.doury@gmail.com>
2019-12-18 18:03:42 +0000

    Deploying to gh-pages from master b2a9061fec400c234f49f6931f07c8c5a3c553ee

---
+++ precache-manifest.8d363d60e7ea55df185b8c796140428b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564f58c7d7e75c63f4fdb2d6b43d280f",
+    "url": "/docavenue/index.html"
+  },
+  {
+    "revision": "f35ab549628d9c83029c",
+    "url": "/docavenue/static/css/main.fc3c6e5a.chunk.css"
+  },
+  {
+    "revision": "6b1aac75ea6a368154b2",
+    "url": "/docavenue/static/js/2.7315ce42.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/docavenue/static/js/2.7315ce42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f35ab549628d9c83029c",
+    "url": "/docavenue/static/js/main.71726561.chunk.js"
+  },
+  {
+    "revision": "898c029dd7fb5b33c25d",
+    "url": "/docavenue/static/js/runtime-main.3238901e.js"
+  },
+  {
+    "revision": "f3fb0c41a792d9ecd107a2d48e847574",
+    "url": "/docavenue/static/media/Spinner.f3fb0c41.svg"
+  }
+]);
